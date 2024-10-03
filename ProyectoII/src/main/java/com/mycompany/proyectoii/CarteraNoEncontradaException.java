c93eced1b2545c119f4520e292aca4f6a057e31a
llulz <llulz@DESKTOP-59PKABE>
2024-10-03 18:31:15 -0300

    Exepcion creada

---
+++ ProyectoII/src/main/java/com/mycompany/proyectoii/CarteraNoEncontradaException.java
@@ -0,0 +1,8 @@
+package com.mycompany.proyectoii;
+
+public class CarteraNoEncontradaException extends Exception {
+    public CarteraNoEncontradaException(String mensaje) {
+        super(mensaje);
+    }
+}
+
